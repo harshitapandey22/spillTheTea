6be9663620573144450863c5f8ff211e48915d80
harshitapandey22 <pharshita588@gmail.com>
2025-10-06 18:07:07 +0530

    fixed error

--- mongodb/index.js
+++ mongodb/index.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://neerajmain704_db_user:8WY0F6Dmya1Edw7G@spillthetea.qwavc4y.mongodb.net/");
+
+mongoose.connect("mongodb+srv://neerajmain704_db_user:8WY0F6Dmya1Edw7G@spillthetea.qwavc4y.mongodb.net/")
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
 const userSchema = new mongoose.Schema({
-  username:{type:String},
-  password:{type:String},
-  email:{type:String},
-})
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  email:    { type: String, required: true }
+});
 
-const User =  mongoose.model("User",userSchema);
+const User = mongoose.model("User", userSchema);
+
 module.exports = {
   User
-}
+};
--- server.js
+++ server.js
@@ -1,33 +1,36 @@
 const express = require("express");
-const {User} = require("./mongodb");
+const { User } = require("./mongodb");
 const app = express();
 app.use(express.json());
-const PORT = 8080;
+const PORT = 3000;
 
-app.post("/signup", async (req,res)=>{
+app.post("/signup", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
   const email = req.body.email;
 
-  if(!username){
-    return res.status(410).json({msg:"no username"});
+  if (!username) {
+    return res.status(400).json({ msg: "no username" });
   }
-  if(!password){
-    return res.status(410).json({msg:"no password"});
+  if (!password) {
+    return res.status(400).json({ msg: "no password" });
   }
-  if(!email){
-    return res.status(410).json({msg:"no email"});
+  if (!email) {
+    return res.status(400).json({ msg: "no email" });
   }
 
-  const checkingEmail = await User.findOne({email})
-  if(checkingEmail){
-    return res.status(400).json({msg:"email already exists"});
+  try {
+    const checkingEmail = await User.findOne({ email });
+    if (checkingEmail) {
+      return res.status(400).json({ msg: "email already exists" });
+    }
+    const newUser = await User.create({ username, password, email });
+    return res.status(200).json({ msg: "account created" });
+  } catch (err) {
+    return res.status(500).json({ msg: "server error", error: err.message });
   }
-  const newUser = await User.create({username,password,email});
-  return res.status(200).json({msg:"account created"});
+});
 
-})
-
-app.listen(PORT,(req,res)=>{
+app.listen(PORT, () => {
   console.log("server is running");
-})
+});
